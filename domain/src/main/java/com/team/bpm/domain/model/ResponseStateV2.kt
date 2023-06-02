7d6452bca10dce1a433c875ee51abd3997443224
jangjh123 <gn0001231@gmail.com>
2023-06-02 11:58:23 +0900

    [BPM-209] ResponseStateV2 삭제 처리
    ResponseState 는 추후 삭제 예정.

--- domain/src/main/java/com/team/bpm/domain/model/ResponseStateV2.kt
+++
@@ -1,7 +0,0 @@
-package com.team.bpm.domain.model
-
-sealed class ResponseStateV2<out T> {
-    data class Success<T>(val data: T) : ResponseStateV2<T>()
-    data class Error(val code: String?) : ResponseStateV2<Nothing>()
-}
-
